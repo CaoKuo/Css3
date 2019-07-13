08f1031c12c6257aa5837f17a6ba226b2d2cbb0d
CaoKuo <1150729044@qq.com>
2019-07-13 17:18:07 +0800

    Css3

---
+++ css3-demo1/index.js
@@ -0,0 +1,11 @@
+
+$('.pic').click(function () {
+    $('.wrapper').addClass('activeWrapper');
+    $(this).addClass('active');
+})
+
+$('.close').click(function (e) {
+    e.stopPropagation();
+    $('.activeWrapper').removeClass('activeWrapper');
+    $('.active').removeClass('active');
+})
